bbd2b97d79fc263d1c7ff7b1882725709a96e231
Justin Wolf <roadrunner2348@gmail.com>
2015-03-21 15:23:11 -0400

    Fixed machine variable not getting loaded

--- modelgraph.py
+++ modelgraph.py
@@ -3,18 +3,27 @@ from yapsy.PluginManager import PluginManager
 from django.template import loader, Context
 from django.db.models import Count
 from server.models import *
+from django.shortcuts import get_object_or_404
+import server.utils as utils
 
 class ModelGraph(IPlugin):
 	def show_widget(self, page, machines=None, theid=None):
 
 		if page == 'front':
 			t = loader.get_template('justinwolf/modelgraph/templates/front.html')
+			if not machines:
+				machines = Machine.objects.all()
 
 		if page == 'bu_dashboard':
 			t = loader.get_template('justinwolf/modelgraph/templates/id.html')
+			if not machines:
+				machines = utils.getBUmachines(theid)
 
 		if page == 'group_dashboard':
 			t = loader.get_template('justinwolf/modelgraph/templates/id.html')
+			if not machines:
+				machine_group = get_object_or_404(MachineGroup, pk=theid)
+				machines = Machine.objects.filter(machine_group=machine_group)
 
 		if machines:
 			model_info = machines.values('machine_model').annotate(count=Count('machine_model')).order_by()
